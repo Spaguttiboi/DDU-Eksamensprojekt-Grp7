029318580957f59a5f9b33297d724e83974830f5
malt3306 <malthed2003@gmail.com>
2022-04-22 16:01:27 +0200

    small gameplay fixes

---
+++ DDU Eksamensprojekt Grp 7/Assets/Scripts/BackgroundMover.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMover : MonoBehaviour
+{
+    public GameObject player;
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, 0);
+    }
+}
