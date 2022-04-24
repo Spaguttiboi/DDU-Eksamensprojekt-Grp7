cb0bdc8594576d763e68ce1ddf47cd9d5eefb03b
Spaguttiboi <markus.v.kleist@gmail.com>
2022-04-24 18:24:53 +0200

    Digital art for mood and other fixes

---
+++ DDU Eksamensprojekt Grp 7/Assets/Scripts/MoodSymbolScript.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoodSymbolScript : MonoBehaviour
+{
+    SpriteRenderer moodSpriteRenderer;
+    public Sprite[] moodSprites = new Sprite[4];
+    private PlayerMovement movementScript;
+
+    bool numb;
+    bool angry;
+    bool anxious;
+    bool fear;
+
+    float duration = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementScript = gameObject.GetComponentInParent<PlayerMovement>();
+        moodSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        numb = movementScript.numb;
+        angry = movementScript.angry;
+        anxious = movementScript.anxious;
+        fear = movementScript.fear;
+
+        SpriteChanger(numb, angry, anxious, fear, moodSpriteRenderer);
+    }
+
+    void SpriteChanger(bool numb, bool angry, bool anxious, bool fear, SpriteRenderer moodSpriteRenderer)
+    {
+        if (Input.GetKey(KeyCode.Alpha1) && numb == false)
+        {
+            moodSpriteRenderer.sprite = moodSprites[0];
+            StartCoroutine(FadeSpriteOut(moodSpriteRenderer));
+        }
+        if (Input.GetKey(KeyCode.Alpha2) && angry == false)
+        {
+            moodSpriteRenderer.sprite = moodSprites[1];
+            StartCoroutine(FadeSpriteOut(moodSpriteRenderer));
+        }
+        if (Input.GetKey(KeyCode.Alpha3) && anxious == false)
+        {
+            moodSpriteRenderer.sprite = moodSprites[2];
+            StartCoroutine(FadeSpriteOut(moodSpriteRenderer));
+        }
+        if (Input.GetKey(KeyCode.Alpha4) && fear == false)
+        {
+            moodSpriteRenderer.sprite = moodSprites[3];
+            StartCoroutine(FadeSpriteOut(moodSpriteRenderer));
+        }
+    }
+
+    IEnumerator FadeSpriteOut(SpriteRenderer currentSprite)
+    {
+        float counter = 0;
+        //Get current color
+        Color spriteColor = currentSprite.material.color;
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            //Fade from 1 to 0
+            float alpha = Mathf.Lerp(1, 0, counter / duration);
+
+            //Change alpha only
+            currentSprite.color = new Color(spriteColor.r, spriteColor.g, spriteColor.b, alpha);
+            //Wait for a frame
+            yield return null;
+        }
+    }
+}
--- DDU Eksamensprojekt Grp 7/Assets/Scripts/PlayerMovement.cs
+++ DDU Eksamensprojekt Grp 7/Assets/Scripts/PlayerMovement.cs
@@ -16,8 +16,8 @@ public class PlayerMovement : MonoBehaviour
     float jumpHeight = 10f;
     float gravity = -9.82f;
 
-    bool plswork = false;
-    float timework = 2f;
+    bool jumpSwitch = false;
+    float timeJumpSwitch = 2f;
 
     public float pushLength = 0.2f;
     public float distance = 1f;
@@ -28,7 +28,7 @@ public class PlayerMovement : MonoBehaviour
     public bool numb = true;
     public bool angry = false;
     public bool anxious = false;
-    public bool scared = false;
+    public bool fear = false;
 
     Camera playerCamera;
     Animator MoveAnimation;
@@ -61,24 +61,24 @@ public class PlayerMovement : MonoBehaviour
         {
             rigidbody.velocity = Vector2.up * Mathf.Sqrt((jumpHeight * rigidbody.gravityScale) * (-2) * gravity);
             MoveAnimation.SetBool("IsJumping", true);
-            plswork = true;
+            jumpSwitch = true;
 
             audioSource.loop = false;
             audioSource.clip = jumpSound;
             audioSource.Play();
         }
-        if (plswork && IsGrounded() && Time.time > timework)
+        if (jumpSwitch && IsGrounded() && Time.time > timeJumpSwitch)
         {
-            timework = Time.time + 1f;
+            timeJumpSwitch = Time.time + 1f;
             MoveAnimation.SetBool("IsJumping", false);
-            plswork = false;
+            jumpSwitch = false;
 
             audioSource.loop = false;
             audioSource.clip = landSound;
             audioSource.Play();
         }
 
-        temperaryMovementSystem();
+        MoodChooser();
     }
 
     private bool IsGrounded()
@@ -86,80 +86,125 @@ public class PlayerMovement : MonoBehaviour
         return Physics2D.Raycast(transform.position, Vector2.down, 1.3f, platformLayerMask) != false;
     }
 
-    void MoveDirection()
-    {
-        float direction = Input.GetAxisRaw("Horizontal");
-
-        if (direction == -1)
-            rigidbody.velocity = new Vector2(-moveSpeed, rigidbody.velocity.y);
-        else if (direction == 1)
-            rigidbody.velocity = new Vector2(+moveSpeed, rigidbody.velocity.y);
-        else
-            rigidbody.velocity = new Vector2(0, rigidbody.velocity.y);
-
-        FlipPlayerModel(direction);
-        MoveAnimation.SetFloat("Speed", Mathf.Abs(direction));
-    }
-
-    void FlipPlayerModel(float direction)
-    {
-        if (direction == 1)
-        {
-            Vector3 scale = transform.localScale;
-            scale.x = -1f;
-            transform.localScale = scale;
-        }
-        else if (direction == -1)
-        {
-            Vector3 scale = transform.localScale;
-            scale.x = 1f;
-            transform.localScale = scale;
-        }
-    }
-
     void MoodChooser()
     {
+        //Mood system
         if (Input.GetKey(KeyCode.Alpha1) && numb == false)
         {
             numb = true;
             angry = false;
             anxious = false;
-            scared = false;
+            fear = false;
 
             audioSource.loop = false;
             audioSource.clip = numbSound;
             audioSource.Play();
+
+            MoveAnimation.SetBool("IsNumb", true);
+            MoveAnimation.SetBool("IsAngry", false);
+            MoveAnimation.SetBool("IsAnxiety", false);
+            MoveAnimation.SetBool("IsFear", false);
         }
         if (Input.GetKey(KeyCode.Alpha2) && angry == false)
         {
             numb = false;
             angry = true;
             anxious = false;
-            scared = false;
+            fear = false;
 
             audioSource.loop = false;
             audioSource.clip = angrySound;
             audioSource.Play();
+
+            MoveAnimation.SetBool("IsNumb", false);
+            MoveAnimation.SetBool("IsAngry", true);
+            MoveAnimation.SetBool("IsAnxiety", false);
+            MoveAnimation.SetBool("IsFear", false);
         }
         if (Input.GetKey(KeyCode.Alpha3) && anxious == false)
         {
             numb = false;
             angry = false;
             anxious = true;
-            scared = false;
+            fear = false;
 
             audioSource.loop = false;
             audioSource.clip = anxietySound;
             audioSource.Play();
+
+            MoveAnimation.SetBool("IsNumb", false);
+            MoveAnimation.SetBool("IsAngry", false);
+            MoveAnimation.SetBool("IsAnxiety", true);
+            MoveAnimation.SetBool("IsFear", false);
         }
-        if (Input.GetKey(KeyCode.Alpha4) && scared == false)
+        if (Input.GetKey(KeyCode.Alpha4) && fear == false)
         {
             numb = false;
             angry = false;
             anxious = false;
-            scared = true;
+            fear = true;
 
+            MoveAnimation.SetBool("IsNumb", false);
+            MoveAnimation.SetBool("IsAngry", false);
+            MoveAnimation.SetBool("IsAnxiety", false);
+            MoveAnimation.SetBool("IsFear", true);
+        }
 
+        MoveDirection(numb, angry, anxious, fear);
+    }
+
+    void MoveDirection(bool numb, bool angry, bool anxious, bool fear)
+    {
+        float direction = Input.GetAxisRaw("Horizontal");
+
+        if (direction == -1 && angry == false || direction == 1 && angry)
+            rigidbody.velocity = new Vector2(-moveSpeed, rigidbody.velocity.y);
+        else if (direction == 1 && angry == false || direction == -1 && angry)
+            rigidbody.velocity = new Vector2(+moveSpeed, rigidbody.velocity.y);
+        else
+            rigidbody.velocity = new Vector2(0, rigidbody.velocity.y);
+
+        //Change speed when scared
+        if (numb || angry || anxious)
+            moveSpeed = playerSpeed;
+        else if (fear)
+        {
+            moveSpeed = scaredPlayerSpeed;
+
+            audioSource.loop = true;
+            audioSource.clip = runSound;
+            audioSource.Play();
+        }
+        //Change jump height when anxious
+        if (numb || angry || fear)
+        {
+            jumpHeight = playerJumpHeight;
+            playerCamera.orthographicSize = 5;
+        }
+        else if (anxious)
+        {
+            jumpHeight = anxiousPlayerJumpHeight;
+            playerCamera.orthographicSize = 3;
+        }
+
+        //Visual system
+        FlipPlayerModel(direction, angry);
+        MoveAnimation.SetFloat("Speed", Mathf.Abs(direction));
+    }
+
+    void FlipPlayerModel(float direction, bool angry)
+    {
+        if (direction == 1 && angry == false || direction == -1 && angry)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x = -1f;
+            transform.localScale = scale;
+        }
+        else if (direction == -1 && angry == false || direction == 1 && angry)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x = 1f;
+            transform.localScale = scale;
         }
     }
 
@@ -177,7 +222,7 @@ public class PlayerMovement : MonoBehaviour
             numb = true;
             angry = false;
             anxious = false;
-            scared = false;
+            fear = false;
 
             audioSource.loop = false;
             audioSource.clip = numbSound;
@@ -193,7 +238,7 @@ public class PlayerMovement : MonoBehaviour
             numb = false;
             angry = true;
             anxious = false;
-            scared = false;
+            fear = false;
 
             audioSource.loop = false;
             audioSource.clip = angrySound;
@@ -209,7 +254,7 @@ public class PlayerMovement : MonoBehaviour
             numb = false;
             angry = false;
             anxious = true;
-            scared = false;
+            fear = false;
 
             audioSource.loop = false;
             audioSource.clip = anxietySound;
@@ -220,12 +265,12 @@ public class PlayerMovement : MonoBehaviour
             MoveAnimation.SetBool("IsAnxiety", true);
             MoveAnimation.SetBool("IsFear", false);
         }
-        if (Input.GetKey(KeyCode.Alpha4) && scared == false)
+        if (Input.GetKey(KeyCode.Alpha4) && fear == false)
         {
             numb = false;
             angry = false;
             anxious = false;
-            scared = true;
+            fear = true;
 
             MoveAnimation.SetBool("IsNumb", false);
             MoveAnimation.SetBool("IsAngry", false);
@@ -245,7 +290,7 @@ public class PlayerMovement : MonoBehaviour
         //Change speed when scared
         if (numb || angry || anxious)
             moveSpeed = playerSpeed;
-        else if (scared)
+        else if (fear)
         {
             moveSpeed = scaredPlayerSpeed;
 
@@ -254,7 +299,7 @@ public class PlayerMovement : MonoBehaviour
             audioSource.Play();
         }
         //Change jump height when anxious
-        if (numb || angry || scared)
+        if (numb || angry || fear)
         {
             jumpHeight = playerJumpHeight;
             playerCamera.orthographicSize = 5;
@@ -265,12 +310,8 @@ public class PlayerMovement : MonoBehaviour
             playerCamera.orthographicSize = 3;
         }
 
-
-
-
-
         //Visual system
-        FlipPlayerModel(direction);
+        FlipPlayerModel(direction, angry);
         MoveAnimation.SetFloat("Speed", Mathf.Abs(direction));
     }
 }
